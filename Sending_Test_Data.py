9019eec17ddf8d1912ff43a124b8cce44c776359
Eric Dundas <kuribodragon@gmail.com>
2023-11-19 22:03:05 -0600

    Update Sending_Test_Data.py

--- Sending_Test_Data.py
+++ Sending_Test_Data.py
@@ -50,6 +50,6 @@ iotc.send_property({
 
 while iotc.is_connected():
     # send_command("roll", "1d6")
-    send_command("feature", {"filepath":"commandListener.py", "view":True})
+    send_command("feature", {"filepath":"5etools/data/spells/spells-phb.json", "view":True})
     # print (_command_url())
     time.sleep(4)
