19d9012fae5ace127d7d801ef163954cbf7b454c
Nazrin H. Nazarudin <nazrin.hakim24@gmail.com>
2023-11-30 19:19:10 -0600

    Removed while loop

--- App/current/Sending_Test_Data.py
+++ App/current/Sending_Test_Data.py
@@ -48,8 +48,8 @@ iotc.send_property({
     "LastTurnedOn": time.time()
 })
 
-while iotc.is_connected():
-    # send_command("roll", "1d6")
-    send_command("feature", {"filepath":"./5etools/data/spells/spells-phb.json", "featurename":"Magic Missile", "view":True})
-    # print (_command_url())
-    time.sleep(4)
+#if iotc.is_connected():
+# send_command("roll", "1d6")
+send_command("feature", {"filepath":"./5etools/data/spells/spells-phb.json", "featurename":"Magic Missile", "view":True})
+# print (_command_url())
+time.sleep(4)
